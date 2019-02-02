e68df234ed5fe9e7da9266cfae55af9e8a44e014
Varga Zsolt <shadowvzs@hotmail.com>
2019-02-02 16:28:09 +0200

    added buildSchema from graphQl for build schema

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,17 +3,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // middleware which convert the incoming json to valid graphql query
 const graphqlHttp = require('express-graphql');
+// buildSchema help to build a js object literal
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 // parse the request body which was sent from frontend
 app.use(bodyParser.json());
 
-// a basic route
-app.get('/', (req, res, next) => {
-    res.send('hello asd'); 
-})
+// graphql endpoint where graphql middleware will proccess the json to query
+// we must pass an object where we declare:
+// - scheme we create with graphql package
+// - rootValue is resolver function which must match with schema enpoint names
+app.get('/graphql', graphqlHttp({
+    // buildSchema takes string and build javascript schema object from it
+    // we use backticket for multiline purpose
+    schema: buildSchema(`
+        schema {
+            query:
+            mutation: 
+        }
+    `),
+    rootValue: {}
+}));
 
 // start/listen server to a port
 app.listen(3000);
 
+
+/**
+ * Note: 
+ * In schema the:
+ *    - query is the Get like request where we need data
+ *    - mutation is the post/put/patch/delete like request where we must change data
+ *    -  is websocket like requests 
+ * 
+ * 
+ */
